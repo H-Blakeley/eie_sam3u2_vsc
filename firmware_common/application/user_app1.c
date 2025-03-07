20b83817ea933d9bfb1db953470266cbd9187ed5
H-Blakeley <hannah.blakeley@ucalgary.ca>
2025-03-06 20:31:27 -0700

    Move loading screens to functions

--- firmware_common/application/user_app1.c
+++ firmware_common/application/user_app1.c
@@ -81,6 +81,162 @@ Function Definitions
 /*! @publicsection */                                                                                            
 /*--------------------------------------------------------------------------------------------------------------------*/
 
+void LoadLittleDove(u8 u8RowState) {
+    // load row arrow at 1
+    static PixelAddressType sPointWSLoc = {U8_LCD_SMALL_FONT_LINE6, U16_LCD_LEFT_MOST_COLUMN};
+    u8 au8PointWSStr[] = "W>";
+    LcdLoadString(au8PointWSStr, LCD_FONT_SMALL, &sPointWSLoc);
+
+    // load row arrow at 2
+    u8 au8TestString[] = "<R";
+    static PixelAddressType sTestLoc = {U8_LCD_SMALL_FONT_LINE6 - 12, (U16_LCD_RIGHT_MOST_COLUMN) - 13};
+    LcdLoadString(au8TestString, LCD_FONT_SMALL, &sTestLoc);
+  
+    // load check boxes
+  
+    static PixelBlockType sCheckBox;
+    sCheckBox.u16RowStart = 0;
+    sCheckBox.u16ColumnStart = 12;
+    sCheckBox.u16RowSize = 8; // pixels tall 
+    sCheckBox.u16ColumnSize = 10; // pixels wide
+  
+    for(u8 i = 0; i < 5; i++){
+      LcdLoadBitmap(&aau8StitchK, &sCheckBox);
+      sCheckBox.u16RowStart +=12;
+    } 
+  
+    // load grid
+    // grid row 1, 3, 5 (purl all stitches)
+  
+    static PixelBlockType sPRow;
+    sPRow.u16RowStart = U8_LCD_SMALL_FONT_LINE6;
+    sPRow.u16ColumnStart = 23;
+    sPRow.u16RowSize = 8;
+    sPRow.u16ColumnSize = 10;
+  
+    for(u8 h = 0; h < 3; h++){
+      for(u8 i=0; i< 9; i++)
+      {
+        LcdLoadBitmap(&aau8StitchP, &sPRow);
+        sPRow.u16ColumnStart +=10;
+      }
+      sPRow.u16RowStart -= 24;
+      sPRow.u16ColumnStart = 23;
+    }
+
+  
+    // grid row 2
+    static PixelBlockType sRow2;
+    sRow2.u16RowStart = (U8_LCD_SMALL_FONT_LINE6)-12;
+    sRow2.u16ColumnStart = 23;
+    sRow2.u16RowSize = 8;
+    sRow2.u16ColumnSize = 10;
+
+    LcdLoadBitmap(&aau8StitchK, &sRow2);
+    sRow2.u16ColumnStart+=10;
+    LcdLoadBitmap(&aau8StitchYO, &sRow2);
+    sRow2.u16ColumnStart+=10;
+    LcdLoadBitmap(&aau8StitchK, &sRow2);
+    sRow2.u16ColumnStart+=10;
+    LcdLoadBitmap(&aau8StitchK, &sRow2);
+    sRow2.u16ColumnStart+=10;
+    LcdLoadBitmap(&aau8StitchSSK, &sRow2);
+    sRow2.u16ColumnStart+=10;
+    LcdLoadBitmap(&aau8StitchK2TOG, &sRow2);
+    sRow2.u16ColumnStart+=10;
+    LcdLoadBitmap(&aau8StitchK, &sRow2);
+    sRow2.u16ColumnStart+=10;
+    LcdLoadBitmap(&aau8StitchK, &sRow2);
+    sRow2.u16ColumnStart+=10;
+    LcdLoadBitmap(&aau8StitchYO, &sRow2);
+  
+  
+    // grid row 4
+    static PixelBlockType sRow4;
+    sRow4.u16RowStart = (U8_LCD_SMALL_FONT_LINE6)-36;
+    sRow4.u16ColumnStart = 23;
+    sRow4.u16RowSize = 8;
+    sRow4.u16ColumnSize = 10;
+
+    LcdLoadBitmap(&aau8StitchYO, &sRow4);
+    sRow4.u16ColumnStart+=10;
+    LcdLoadBitmap(&aau8StitchK, &sRow4);
+    sRow4.u16ColumnStart+=10;
+    LcdLoadBitmap(&aau8StitchK, &sRow4);
+    sRow4.u16ColumnStart+=10;
+    LcdLoadBitmap(&aau8StitchSSK, &sRow4);
+    sRow4.u16ColumnStart+=10;
+    LcdLoadBitmap(&aau8StitchK2TOG, &sRow4);
+    sRow4.u16ColumnStart+=10;
+    LcdLoadBitmap(&aau8StitchK, &sRow4);
+    sRow4.u16ColumnStart+=10;
+    LcdLoadBitmap(&aau8StitchK, &sRow4);
+    sRow4.u16ColumnStart+=10;
+    LcdLoadBitmap(&aau8StitchYO, &sRow4);
+    sRow4.u16ColumnStart+=10;
+    LcdLoadBitmap(&aau8StitchK, &sRow4);
+  
+    // load lines marking place at row 1
+  
+    //static PixelBlockType sUpperRowMark = {(u8)60, (u8)23, (u8)36, (u8)1};
+    //LcdLoadBitmap(aau8RowMark, &sUpperRowMark);
+  
+  
+    //static PixelAddressType sRowMarkUpper = {U8_LCD_SMALL_FONT_LINE6, U16_LCD_LEFT_MOST_COLUMN};
+    //u8 au8RowMark[] = "_____________";
+    //LcdLoadString(au8RowMark, LCD_FONT_SMALL, &sRowMarkUpper);
+    //static PixelAddressType sRowMarkLower = {U8_LCD_SMALL_FONT_LINE7, U16_LCD_LEFT_MOST_COLUMN};
+    //LcdLoadString(au8RowMark, LCD_FONT_SMALL, &sRowMarkLower);
+    
+    // load rectangle around grid
+
+}
+
+void LoadLegend(void)
+{
+
+  // Load title strings
+  static PixelAddressType sTitleStr1 = {U8_LCD_SMALL_FONT_LINE0, U16_LCD_LEFT_MOST_COLUMN};
+  u8 au8TitleStr1[] = "Pattern Repeat Chart";
+  static PixelAddressType sTitleStr2 = {U8_LCD_SMALL_FONT_LINE1, U16_LCD_LEFT_MOST_COLUMN};
+  u8 au8TitleStr2[] = "Little Dove Blanket";
+  static PixelAddressType sTitleStr3 = {U8_LCD_SMALL_FONT_LINE3, U16_LCD_LEFT_MOST_COLUMN};
+  u8 au8TitleStr3[] = "Legend";
+
+  LcdLoadString(au8TitleStr1, LCD_FONT_SMALL, &sTitleStr1);
+  LcdLoadString(au8TitleStr2, LCD_FONT_SMALL, &sTitleStr2);
+  LcdLoadString(au8TitleStr3, LCD_FONT_SMALL, &sTitleStr3);
+
+
+  // Load legend strings
+  static PixelAddressType sLabelAddress = {U8_LCD_SMALL_FONT_LINE6, U16_LCD_LEFT_MOST_COLUMN};
+  u8 au8LegendLabels[] = "K   P  YO  SSK  K2TOG";
+  LcdLoadString(au8LegendLabels, LCD_FONT_SMALL, &sLabelAddress);
+  
+
+  // Load stitch bitmaps above the text
+  static PixelBlockType sStsAddress;
+  sStsAddress.u16RowStart = 38;
+  sStsAddress.u16ColumnStart = 0;
+  sStsAddress.u16RowSize = 8; // pixels tall 
+  sStsAddress.u16ColumnSize = 10; // pixels wide
+
+  LcdLoadBitmap(&aau8StitchK, &sStsAddress);
+  sStsAddress.u16ColumnStart +=20;
+
+  LcdLoadBitmap(&aau8StitchP, &sStsAddress);
+  sStsAddress.u16ColumnStart +=20;
+
+  LcdLoadBitmap(&aau8StitchYO, &sStsAddress);
+  sStsAddress.u16ColumnStart +=30;
+
+  LcdLoadBitmap(&aau8StitchSSK, &sStsAddress);
+  sStsAddress.u16ColumnStart +=33;
+  
+  LcdLoadBitmap(&aau8StitchK2TOG, &sStsAddress);
+
+}
+
 /*--------------------------------------------------------------------------------------------------------------------*/
 /*! @protectedsection */                                                                                            
 /*--------------------------------------------------------------------------------------------------------------------*/
@@ -102,109 +258,11 @@ Promises:
 */
 void UserApp1Initialize(void)
 {
+  
   LcdClearScreen();
-
+  
   // ~Load static pattern. 
-  
-  // load row arrow at 1
-  static PixelAddressType sPointWSLoc = {U8_LCD_SMALL_FONT_LINE6, U16_LCD_LEFT_MOST_COLUMN};
-  u8 au8PointWSStr[] = "W>";
-  LcdLoadString(au8PointWSStr, LCD_FONT_SMALL, &sPointWSLoc);
-
-  //load column arrow at first stitch
-  static PixelAddressType sPointStColLoc = {U8_LCD_SMALL_FONT_LINE7, 15};
-  u8 au8PointStColStr[] = "^";
-  LcdLoadString(au8PointStColStr, LCD_FONT_SMALL, &sPointStColLoc);
-
-  // load check boxes
-
-  static PixelBlockType sCheckBox;
-  sCheckBox.u16RowStart = 0;
-  sCheckBox.u16ColumnStart = 12;
-  sCheckBox.u16RowSize = 8; // pixels tall 
-  sCheckBox.u16ColumnSize = 10; // pixels wide
-
-  for(u8 i = 0; i < 5; i++){
-    LcdLoadBitmap(&aau8StitchK, &sCheckBox);
-    sCheckBox.u16RowStart +=12;
-  } 
-
-  // load grid
-  // grid row 1, 3, 5 (purl all stitches)
-
-  static PixelBlockType sPRow = {U8_LCD_SMALL_FONT_LINE6, 23, 8, 10}; 
-
-  for(u8 h = 0; h < 3; h++){
-    for(u8 i=0; i< 9; i++)
-    {
-      LcdLoadBitmap(&aau8StitchP, &sPRow);
-      sPRow.u16ColumnStart +=10;
-    }
-    sPRow.u16RowStart -= 24;
-    sPRow.u16ColumnStart = 23;
-
-  }
-  
-
-  // grid row 2
-  static PixelBlockType sRow2 = {(U8_LCD_SMALL_FONT_LINE6)-12, 23, 8, 10};
-  LcdLoadBitmap(&aau8StitchK, &sRow2);
-  sRow2.u16ColumnStart+=10;
-  LcdLoadBitmap(&aau8StitchYO, &sRow2);
-  sRow2.u16ColumnStart+=10;
-  LcdLoadBitmap(&aau8StitchK, &sRow2);
-  sRow2.u16ColumnStart+=10;
-  LcdLoadBitmap(&aau8StitchK, &sRow2);
-  sRow2.u16ColumnStart+=10;
-  LcdLoadBitmap(&aau8StitchSSK, &sRow2);
-  sRow2.u16ColumnStart+=10;
-  LcdLoadBitmap(&aau8StitchK2TOG, &sRow2);
-  sRow2.u16ColumnStart+=10;
-  LcdLoadBitmap(&aau8StitchK, &sRow2);
-  sRow2.u16ColumnStart+=10;
-  LcdLoadBitmap(&aau8StitchK, &sRow2);
-  sRow2.u16ColumnStart+=10;
-  LcdLoadBitmap(&aau8StitchYO, &sRow2);
-
-
-  // grid row 4
-  static PixelBlockType sRow4 = {(U8_LCD_SMALL_FONT_LINE6)-36, 23, 8, 10};
-  LcdLoadBitmap(&aau8StitchYO, &sRow4);
-  sRow4.u16ColumnStart+=10;
-  LcdLoadBitmap(&aau8StitchK, &sRow4);
-  sRow4.u16ColumnStart+=10;
-  LcdLoadBitmap(&aau8StitchK, &sRow4);
-  sRow4.u16ColumnStart+=10;
-  LcdLoadBitmap(&aau8StitchSSK, &sRow4);
-  sRow4.u16ColumnStart+=10;
-  LcdLoadBitmap(&aau8StitchK2TOG, &sRow4);
-  sRow4.u16ColumnStart+=10;
-  LcdLoadBitmap(&aau8StitchK, &sRow4);
-  sRow4.u16ColumnStart+=10;
-  LcdLoadBitmap(&aau8StitchK, &sRow4);
-  sRow4.u16ColumnStart+=10;
-  LcdLoadBitmap(&aau8StitchYO, &sRow4);
-  sRow4.u16ColumnStart+=10;
-  LcdLoadBitmap(&aau8StitchK, &sRow4);
-
-
-  // load lines marking place at row 1
-
-  //static PixelBlockType sUpperRowMark = {(u8)60, (u8)23, (u8)36, (u8)1};
-  //LcdLoadBitmap(aau8RowMark, &sUpperRowMark);
-
-
-  //static PixelAddressType sRowMarkUpper = {U8_LCD_SMALL_FONT_LINE6, U16_LCD_LEFT_MOST_COLUMN};
-  //u8 au8RowMark[] = "_____________";
-  //LcdLoadString(au8RowMark, LCD_FONT_SMALL, &sRowMarkUpper);
-  //static PixelAddressType sRowMarkLower = {U8_LCD_SMALL_FONT_LINE7, U16_LCD_LEFT_MOST_COLUMN};
-  //LcdLoadString(au8RowMark, LCD_FONT_SMALL, &sRowMarkLower);
-  
-  // load rectangle around grid
-
-
-
-
+  LoadLittleDove(0b00000);
 
   
   /* If good initialization, set state to Idle */
@@ -259,149 +317,12 @@ static void UserApp1SM_Idle(void)
   if(WasButtonPressed(BUTTON0)) {
     LcdClearScreen();
     if(u8Legend_On == 0){
-      
-      // Load title strings
-      static PixelAddressType sTitleStr1 = {U8_LCD_SMALL_FONT_LINE0, U16_LCD_LEFT_MOST_COLUMN};
-      u8 au8TitleStr1[] = "Pattern Repeat Chart";
-      static PixelAddressType sTitleStr2 = {U8_LCD_SMALL_FONT_LINE1, U16_LCD_LEFT_MOST_COLUMN};
-      u8 au8TitleStr2[] = "Little Dove Blanket";
-      static PixelAddressType sTitleStr3 = {U8_LCD_SMALL_FONT_LINE3, U16_LCD_LEFT_MOST_COLUMN};
-      u8 au8TitleStr3[] = "Legend";
-
-      LcdLoadString(au8TitleStr1, LCD_FONT_SMALL, &sTitleStr1);
-      LcdLoadString(au8TitleStr2, LCD_FONT_SMALL, &sTitleStr2);
-      LcdLoadString(au8TitleStr3, LCD_FONT_SMALL, &sTitleStr3);
-
-
-      // Load legend strings
-      static PixelAddressType sLabelAddress = {U8_LCD_SMALL_FONT_LINE6, U16_LCD_LEFT_MOST_COLUMN};
-      u8 au8LegendLabels[] = "K   P  YO  SSK  K2TOG";
-      LcdLoadString(au8LegendLabels, LCD_FONT_SMALL, &sLabelAddress);
-      
-
-      // Load stitch bitmaps above the text
-      static PixelBlockType sStsAddress;
-      sStsAddress.u16RowStart = 38;
-      sStsAddress.u16ColumnStart = 0;
-      sStsAddress.u16RowSize = 8; // pixels tall 
-      sStsAddress.u16ColumnSize = 10; // pixels wide
-
-      LcdLoadBitmap(&aau8StitchK, &sStsAddress);
-      sStsAddress.u16ColumnStart +=20;
-
-      LcdLoadBitmap(&aau8StitchP, &sStsAddress);
-      sStsAddress.u16ColumnStart +=20;
-
-      LcdLoadBitmap(&aau8StitchYO, &sStsAddress);
-      sStsAddress.u16ColumnStart +=30;
-
-      LcdLoadBitmap(&aau8StitchSSK, &sStsAddress);
-      sStsAddress.u16ColumnStart +=33;
-      
-      LcdLoadBitmap(&aau8StitchK2TOG, &sStsAddress);
-
+      LoadLegend();
       u8Legend_On = 1; 
     } // if(u8Legend_On ==0)
     else
     {
-        // load row arrow at 1
-      static PixelAddressType sPointWSLoc = {U8_LCD_SMALL_FONT_LINE6, U16_LCD_LEFT_MOST_COLUMN};
-      u8 au8PointWSStr[] = "W>";
-      LcdLoadString(au8PointWSStr, LCD_FONT_SMALL, &sPointWSLoc);
-
-      // load row arrow at 2
-      u8 au8TestString[] = "<R";
-      static PixelAddressType sTestLoc = {U8_LCD_SMALL_FONT_LINE6 - 12, (U16_LCD_RIGHT_MOST_COLUMN) - 13};
-      LcdLoadString(au8TestString, LCD_FONT_SMALL, &sTestLoc);
-
-      //load column arrow at first stitch
-      static PixelAddressType sPointStColLoc = {U8_LCD_SMALL_FONT_LINE7, 15};
-      u8 au8PointStColStr[] = "^";
-      LcdLoadString(au8PointStColStr, LCD_FONT_SMALL, &sPointStColLoc);
-
-      // load check boxes
-
-      static PixelBlockType sCheckBox;
-      sCheckBox.u16RowStart = 0;
-      sCheckBox.u16ColumnStart = 12;
-      sCheckBox.u16RowSize = 8; // pixels tall 
-      sCheckBox.u16ColumnSize = 10; // pixels wide
-
-      for(u8 i = 0; i < 5; i++){
-       LcdLoadBitmap(&aau8StitchK, &sCheckBox);
-        sCheckBox.u16RowStart +=12;
-      } 
-
-      // load grid
-      // grid row 1, 3, 5 (purl all stitches)
-
-     static PixelBlockType sPRow;
-     sPRow.u16RowStart = U8_LCD_SMALL_FONT_LINE6;
-     sPRow.u16ColumnStart = 23;
-     sPRow.u16RowSize = 8; // pixels tall 
-     sPRow.u16ColumnSize = 10; // pixels wide 
-
-     for(u8 h = 0; h < 3; h++){
-        for(u8 i=0; i< 9; i++)
-        {
-          LcdLoadBitmap(&aau8StitchP, &sPRow);
-          sPRow.u16ColumnStart +=10;
-        }
-        sPRow.u16RowStart -= 24;
-        sPRow.u16ColumnStart = 23;
-     }
-  
-
-      // grid row 2
-      static PixelBlockType sRow2;
-      sRow2.u16RowStart = (U8_LCD_SMALL_FONT_LINE6)-12;
-      sRow2.u16ColumnStart = 23;
-      sRow2.u16RowSize = 8; // pixels tall 
-      sRow2.u16ColumnSize = 10; // pixels wide
-
-      LcdLoadBitmap(&aau8StitchK, &sRow2);
-      sRow2.u16ColumnStart+=10;
-      LcdLoadBitmap(&aau8StitchYO, &sRow2);
-      sRow2.u16ColumnStart+=10;
-      LcdLoadBitmap(&aau8StitchK, &sRow2);
-      sRow2.u16ColumnStart+=10;
-      LcdLoadBitmap(&aau8StitchK, &sRow2);
-      sRow2.u16ColumnStart+=10;
-      LcdLoadBitmap(&aau8StitchSSK, &sRow2);
-      sRow2.u16ColumnStart+=10;
-      LcdLoadBitmap(&aau8StitchK2TOG, &sRow2);
-      sRow2.u16ColumnStart+=10;
-      LcdLoadBitmap(&aau8StitchK, &sRow2);
-      sRow2.u16ColumnStart+=10;
-      LcdLoadBitmap(&aau8StitchK, &sRow2);
-      sRow2.u16ColumnStart+=10;
-      LcdLoadBitmap(&aau8StitchYO, &sRow2);
-
-
-      // grid row 4
-      static PixelBlockType sRow4;
-      sRow4.u16RowStart = (U8_LCD_SMALL_FONT_LINE6)-36;
-      sRow4.u16ColumnStart = 23;
-      sRow4.u16RowSize = 8; // pixels tall 
-      sRow4.u16ColumnSize = 10; // pixels wide
-
-      LcdLoadBitmap(&aau8StitchYO, &sRow4);
-      sRow4.u16ColumnStart+=10;
-      LcdLoadBitmap(&aau8StitchK, &sRow4);
-      sRow4.u16ColumnStart+=10;
-      LcdLoadBitmap(&aau8StitchK, &sRow4);
-      sRow4.u16ColumnStart+=10;
-      LcdLoadBitmap(&aau8StitchSSK, &sRow4);
-      sRow4.u16ColumnStart+=10;
-      LcdLoadBitmap(&aau8StitchK2TOG, &sRow4);
-      sRow4.u16ColumnStart+=10;
-      LcdLoadBitmap(&aau8StitchK, &sRow4);
-      sRow4.u16ColumnStart+=10;
-      LcdLoadBitmap(&aau8StitchK, &sRow4);
-      sRow4.u16ColumnStart+=10;
-      LcdLoadBitmap(&aau8StitchYO, &sRow4);
-      sRow4.u16ColumnStart+=10;
-      LcdLoadBitmap(&aau8StitchK, &sRow4);
+      LoadLittleDove(0b0000);
       u8Legend_On = 0;
     } //else
     ButtonAcknowledge(BUTTON0);
@@ -416,10 +337,6 @@ static void UserApp1SM_Idle(void)
     ButtonAcknowledge(BUTTON1);
     if (!(u8Legend_On)) 
     {
-      for(u8 i=0; i<5; i++){
-        // no wait the xor should only happen in the load pattern function
-      }
-    
       static PixelBlockType sCheckPosition;
       sCheckPosition.u16RowStart = 0;
       sCheckPosition.u16ColumnStart = 12*u8current_row; // check what the row is and adjust column start
@@ -458,10 +375,6 @@ static void UserApp1SM_Idle(void)
     }
   }// if(WasButtonPressed(BUTTON1)
 
-
-
-
-  
   //if(0){ // later, figure out captouch and move this if vertical is moved.
     //static PixelAddressType sPointRS = {U8_LCD_SMALL_FONT_LINE7, U16_LCD_RIGHT_MOST_COLUMN};
     //u8 au8TitleStr3[] = "<R";
